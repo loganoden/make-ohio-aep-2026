9e7b6cf016a4792e3f3d9c4c000c771683be787b
Logan Oden <150753799+loganoden@users.noreply.github.com>
2026-03-07 15:19:51 -0500

    Completed platformio.ini file for project configuration, started working on main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,1 +1,9 @@
 // The main loop: reads sensors, triggers LEDs/Fan, sends MQTT
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <math.h>
+
+// Pin Definitions
