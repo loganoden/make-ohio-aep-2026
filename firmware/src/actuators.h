66ecf301f9462995bbb1b1e6f00d8d4059130d94
Mufasa <mustaphawillisonpersonal@gmail.com>
2026-03-08 12:15:51 -0400

    Modify calculateHazardLevel to accept voltage parameter

--- firmware/src/actuators.h
+++ firmware/src/actuators.h
@@ -7,7 +7,7 @@ extern int currentFanSpeed;
 
 void initActuators();
 void updateWindSimulation();
-float calculateHazardLevel(float temp);
+float calculateHazardLevel(float temp, float volt);
 void updateActuators(float hazardLevel);
 
 #endif
